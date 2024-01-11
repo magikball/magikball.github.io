c227d19d5e1fce56bf0d2b3077b8ff01a489d5db
tanksshot <152533666+tanksshot@users.noreply.github.com>
2024-01-11 20:01:05 +0300

    Add files via upload

---
+++ tanks/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event2_Act30(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#5e5a55'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
